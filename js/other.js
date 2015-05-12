e41d035934af05d585d141e8ec2df9cccb72b925
mhendo <mike@suretybonds.com>
2015-05-12 12:26:19 -0500

    HP Done Except Map Markers and Spotify

---
+++ js/other.js
@@ -0,0 +1,13 @@
+/*$('#picture-set img').on('mouseenter', function(){
+	$(this).animate({
+				width: "300px",
+				height: "500px",
+			}, 200)
+});
+
+$('#picture-set img').on('mouseleave', function(){
+	$(this).animate({
+				width: "174px",
+				height: "290px",
+			}, 200)
+});*/
--- js/skills.js
+++ js/skills.js
@@ -1,11 +1,11 @@
 $('.icons').on('mouseenter', function(){
 	console.log('hover')
 	$(this).children('img').addClass('faded');
-	$(this).children('.content1').fadeIn('fast');
+	$(this).children('.content1, .content2').fadeIn('fast');
 });
 
 $('.icons').on('mouseleave', function(){
 	console.log('hover')
 	$(this).children('img').removeClass('faded');
-	$(this).children('.content1').fadeOut('fast');
+	$(this).children('.content1, .content2').fadeOut('fast');
 });
